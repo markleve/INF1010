24a57f52f7a96e0b8113beaba1b3670dc0e220ff
Marie Klever <marieklever@live.no>
2017-03-10 15:52:26 +0100

    Laget Resept klassen

---
+++ Oblig4/Resept.java
@@ -0,0 +1,11 @@
+
+// kan endre denne til abstact naar jeg har laget de to typene
+// resepter som det skal vaere
+public class Resept {
+  private int antall;
+
+  public Resept() {
+    antall = 0;
+  }
+
+}
