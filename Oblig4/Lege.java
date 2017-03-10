d6f1fce184eae42c84f4374ac347e7aa995fb96b
Marie Klever <marieklever@live.no>
2017-03-10 16:03:30 +0100

    La til navn og metode for aa hente navn

--- Oblig4/Lege.java
+++ Oblig4/Lege.java
@@ -1,3 +1,13 @@
 public class Lege implements Comparable<Lege> {
-  
+  private String navn;   // denne skal vaere unik
+
+  public Lege(String navn) {
+    this.navn = navn;
+  }
+
+  public String hentNavn() { return navn; }
+
+  public int compareTo(Lege annenLege) {
+    return 1;
+  }
 }
