d346e88beaf9dc7cd0eeacdd26e7f6cb8d61f371
Marie Klever <marieklever@live.no>
2017-03-10 16:15:52 +0100

    Testet legge til resepter og hente resepter

--- Oblig4/TestPasient.java
+++ Oblig4/TestPasient.java
@@ -8,6 +8,16 @@ public class TestPasient {
     System.out.println("Adresse: " + pasient.hentGateadresse());
     System.out.println("Postnummer: " + pasient.hentPostnummer() + "\n");
 
+    Resept r1 = new Resept("første", 2);
+    Resept r2 = new Resept("andre", 3);
+    pasient.leggTilResept(r1);
+    pasient.leggTilResept(r2);
+    for (Resept rep: pasient.hentReseptliste()) {
+      System.out.println("Reseptliste: " + rep + "\n");
+    }
+
+
+
     Pasient pasient2 = new Pasient("Marie Klever", 123445697, "Moerveien 36a", 1430);
     System.out.println("Dette er pasient med id: " + pasient2.hentId());
     System.out.println("Navn: " + pasient2.hentNavn());
@@ -21,5 +31,8 @@ public class TestPasient {
     System.out.println("Fødselsnummer: " + pasient3.hentFodselsnummer());
     System.out.println("Adresse: " + pasient3.hentGateadresse());
     System.out.println("Postnummer: " + pasient3.hentPostnummer()+ "\n");
+
+
+
   }
 }
