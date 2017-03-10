a6a8c1adb1ba80b470fa24bde0a1966bc14e0ffd
Marie Klever <marieklever@live.no>
2017-03-10 16:36:36 +0100

    La til nye tester!

--- Oblig4/TestLege.java
+++ Oblig4/TestLege.java
@@ -1,6 +1,22 @@
 
 public class TestLege {
   public static void main(String[] args) {
-    
+    Lege lege = new Lege("Dr. Klever");
+    System.out.println(lege);
+
+
+    Resept r1 = new Resept("første", 2);
+    Resept r2 = new Resept("andre", 3);
+    pasient.leggTilResept(r1);
+    pasient.leggTilResept(r2);
+    System.out.println("\nReseptliste");
+    for (Resept rep: pasient.hentReseptliste()) {
+      System.out.println(rep);
+    }
+
+    Pasient pasient2 = new Pasient("Marie Klever", 123445697, "Moerveien 36a", 1430);
+    System.out.println(pasient2);
+    Pasient pasient3 = new Pasient("Marie Klever", 123445697, "Moerveien 36a", 1430);
+    System.out.println(pasient3);
   }
 }
