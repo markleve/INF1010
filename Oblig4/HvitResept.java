82975a0a5368d921d548841d6be206e3c117a5b6
Marie Klever <marieklever@live.no>
2017-03-11 16:55:28 +0100

    La til toString metoden

--- Oblig4/HvitResept.java
+++ Oblig4/HvitResept.java
@@ -8,4 +8,9 @@ public class HvitResept extends Resept {
     super(legemiddel, lege, pasientId, reit);
     id = idGenerator.getAndIncrement();
   }
+
+  @Override
+  public String toString() {
+    return "Blå resept " + super.toString();
+  }
 }
