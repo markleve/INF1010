83e48aaf6fcc5c58586a0797592cda97f4c81c47
Marie Klever <marieklever@live.no>
2017-03-11 16:55:06 +0100

    Laget underklssen med sin id generator

---
+++ Oblig4/HvitResept.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class HvitResept extends Resept {
+
+  private static final AtomicInteger idGenerator = new AtomicInteger();
+
+  public HvitResept(Legemiddel legemiddel, Lege lege, int pasientId, int reit) {
+    super(legemiddel, lege, pasientId, reit);
+    id = idGenerator.getAndIncrement();
+  }
+}
