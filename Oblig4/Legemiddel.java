94a8b8632fb2432f30992b8241d3598947c63381
Marie Klever <marieklever@live.no>
2017-03-11 15:44:50 +0100

    Laget klassen med input variabler og get metoder for aa faa tak i variablene

---
+++ Oblig4/Legemiddel.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+abstract class Legemiddel {
+
+  // skal variablene vaere protected eller private?
+  // trenger subklassene tilgang til variablene?
+  // kan bruke hent metodene i subkassene for aa gi tilgang til variablene
+  protected String navn;
+  private static AtomicInteger nesteId = new AtomicInteger();
+  protected static int id;    // skal dene gjores finale?? Slik at den ikke kan endre?
+  protected double pris;
+  protected double virkestoff;
+
+  public Legemiddel(String navn, double pris, double virkestoff) {
+    this.navn = navn;
+    this.pris = pris;
+    this.virkestoff = virkestoff;
+    id = nesteId.incrementAndGet();
+  }
+
+  public int hentId() { return id; }
+  public String hentNavn() { return navn; }
+  public double hentPris() { return pris; }
+  public double hentVirkestoff() { return virkestoff; }
+}
