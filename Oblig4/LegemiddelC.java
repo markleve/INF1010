c5a8c4d8f793936122b4e582ac6d58736dc88e11
Marie Klever <marieklever@live.no>
2017-03-11 15:57:04 +0100

    Laget underklasse av Legemiddel med tostring metode

---
+++ Oblig4/LegemiddelC.java
@@ -0,0 +1,11 @@
+
+public class LegemiddelC extends Legemiddel {
+
+  public LegemiddelC(String navn, double pris, double virkestoff) {
+    super(navn, pris, virkestoff);
+  }
+
+  public String toString() {
+    return "Vanlig " + super.toString();
+  }
+}
