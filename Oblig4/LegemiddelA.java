1b4ab4736b95040fe626646c93d8f35f67fb0de0
Marie Klever <marieklever@live.no>
2017-03-11 15:45:17 +0100

    Laget underklasse av Legemiddel med input variabler og get metode

---
+++ Oblig4/LegemiddelA.java
@@ -0,0 +1,12 @@
+
+public class LegemiddelA extends Legemiddel {
+
+  private int styrke;
+
+  public LegemiddelA(String navn, double pris, double virkestoff, int styrke) {
+    super(navn, pris, virkestoff);
+    this.styrke = styrke;
+  }
+
+  public int hentNarkotiskStyrke() { return styrke;}
+}
