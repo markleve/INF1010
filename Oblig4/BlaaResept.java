1236c6ed75a3ec884343dcf0209764169be96761
Marie Klever <marieklever@live.no>
2017-03-11 17:05:09 +0100

    Blå resept

--- Oblig4/BlaaResept.java
+++ Oblig4/BlaaResept.java
@@ -8,7 +8,11 @@ public class BlaaResept extends Resept {
     super(legemiddel, lege, pasientId, reit);
     id = idGenerator.getAndIncrement();
   }
-  public String farge() { return "Blå"; }
+  /**
+  * Returnerer reseptens farge. Enten "blaa" eller "hvit".
+  * @return      reseptens farge
+  */
+  public String farge() { return "blaa"; }
 
   @Override
   public String toString() {
