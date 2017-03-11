2930ce025804ceb8bf1a07a7a1efd9f4a08d6c15
Marie Klever <marieklever@live.no>
2017-03-11 16:52:19 +0100

    Laget klassen og dens id generator

---
+++ Oblig4/BlaaResept.java
@@ -0,0 +1,10 @@
+
+public class BlaaResept extends Resept {
+
+  private static final AtomicInteger idGenerator = new AtomicInteger();
+
+  public BlaaResept(Legemiddel legemiddel, Lege lege, int pasientId, int reit) {
+    super(legemiddel, lege, pasientId, reit);
+    id = idGenerator.getAndIncrement();
+  }
+}
