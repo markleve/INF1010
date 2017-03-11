3032669b76797ef1f5ec6019b2d1b598cfe339a8
Marie Klever <marieklever@live.no>
2017-03-11 17:35:02 +0100

    Testet metodene til reseptene

--- Oblig4/TestResept.java
+++ Oblig4/TestResept.java
@@ -18,7 +18,21 @@ public class TestResept {
     Pasient pasient3 = new Pasient("Markus Johan", 14345697, "Haabakken 32", 4355);
 
     BlaaResept resept1 = new BlaaResept(legemiddelA, lege2, pasient.hentId(), 10);
+    BlaaResept resept2 = new BlaaResept(legemiddelA, lege2, pasient.hentId(), 10);
+    BlaaResept resept3 = new BlaaResept(legemiddelA, lege2, pasient.hentId(), 10);
     System.out.println(resept1);
+    System.out.println(resept2);
+    System.out.println(resept3);
+
+    HvitResept resept4 = new HvitResept(legemiddelA, lege2, pasient.hentId(), 10);
+    HvitResept resept5 = new HvitResept(legemiddelA, lege2, pasient.hentId(), 10);
+    HvitResept resept6 = new HvitResept(legemiddelA, lege2, pasient.hentId(), 10);
+
+    System.out.println(resept4);
+    System.out.println(resept5);
+    System.out.println(resept6);
+
+
 
 
   }
