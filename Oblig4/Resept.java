02e82947362fef289da65aa147a2650c625657b6
Marie Klever <marieklever@live.no>
2017-03-11 17:03:59 +0100

    Laget bruk metoden

--- Oblig4/Resept.java
+++ Oblig4/Resept.java
@@ -20,6 +20,32 @@ public abstract class Resept {
   public int hentPasientId() { return pasientId; }
   public int hentReit() { return reit; }
 
+  /**
+  * Bruker resepten én gang. Returner false om resepten er
+  * oppbrukt, ellers returnerer den true.
+  * @return      om resepten kunne brukes
+  */
+  public boolean bruk() {
+    if(reit > 0) {
+      reit--;
+      return true;
+    } else if(reit == 0) {
+      return false;
+    }
+  }
+
+  /**
+  * Returnerer reseptens farge. Enten "blaa" eller "hvit".
+  * @return      reseptens farge
+  */
+  abstract public String farge();
+
+  /**
+  * Returnerer prisen pasienten maa betale.
+  * @return      prisen pasienten maa betale
+  */
+  abstract public double prisAaBetale();
+
   // skal det vaere en @Override her ???
   public String toString() {
     return "[" + hentId() + "]" + "\nLegemiddel: " + hentLegemiddel()
