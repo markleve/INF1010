b3f9cc610da92a5d98e10cf41ab0caeba596cffc
Marie Klever <marieklever@live.no>
2017-03-11 14:51:27 +0100

    Opprettet Legeliste klassen med forhaands definerte metoder

---
+++ Oblig4/Legeliste.java
@@ -0,0 +1,22 @@
+
+public class Legeliste extends OrdnetLenkeliste<Lege> {
+
+  /**
+   * Soeker gjennom listen etter en lege med samme navn som `navn`
+   * og returnerer legen (uten aa fjerne den fra listen).
+   * Hvis ingen slik lege finnes, returneres `null`.
+   * @param   navn    navnet til legen
+   * @return  legen
+   */
+  public Lege finnLege(String navn) {
+  }
+
+  /**
+   * Returnerer et String[] med navnene til alle legene i listen
+   * i samme rekkefoelge som de staar i listen.
+   * @return array med navn til alle legene
+   */
+  public String[] stringArrayMedNavn() {
+
+  }
+}
