28f70b021e242d98cb5e4d69c77d273cc995afa8
Marie Klever <marieklever@live.no>
2017-03-11 14:40:23 +0100

    La til toString metoden

--- Oblig4/Fastlege.java
+++ Oblig4/Fastlege.java
@@ -7,6 +7,15 @@ public class Fastlege extends Lege implements Kommuneavtale {
     this.avtalenummer = avtalenummer;
   }
 
+  // skal man skrive @Override over metoder fra interface?
+  // man overskriver jo ikke en metode, da en metode ikke er
+  // implementert ennaa. Det er bare signaturen som er laget
+  // i interfacet .... ????
   public int hentAvtalenummer() { return avtalenummer; }
+
+  @Override
+  public String toString() {
+    return super.toString() + " har avtalenummer " + hentAvtalenummer();
+  }
 
 }
