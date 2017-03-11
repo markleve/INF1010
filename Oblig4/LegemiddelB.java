56e46bac318360e34bd5cdedfbe0692285463848
Marie Klever <marieklever@live.no>
2017-03-11 15:50:41 +0100

    Korrigerte type

--- Oblig4/LegemiddelB.java
+++ Oblig4/LegemiddelB.java
@@ -1,7 +1,7 @@
 
 public class LegemiddelB extends Legemiddel {
 
-  private in styrke;
+  private int styrke;
 
   public LegemiddelB(String navn, double pris, double virkestoff, int styrke) {
     super(navn, pris, virkestoff);
